a6ffc247945682ccf3dc246b92d5cdb068961d8e
d-theus <slma0x02@gmail.com>
2015-09-15 10:19:49 +0300

    admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def example
     render text: 'Example'
   end
+
+  def admin?
+    render(
+      file: File.join(Rails.root, 'public/403.html'),
+      status: 403,
+      layout: false
+    )
+  end
 end
