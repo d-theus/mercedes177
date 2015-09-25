ebc22283fb03f4f9d7f3b006091c73a890711ce8
d-theus <slma0x02@gmail.com>
2015-09-25 11:08:02 +0300

    added ui-bootstap & templates_contoller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :items, defaults: { format: :json} do
     resources :photos, only: [:index, :create]
   end
+  scope 'templates' do
+    get ':ctrl/:name', controller: :templates, action: :show
+  end
   get 'items/search', defaults: { format: :json}
   delete 'photos/:id', defaults: { format: :json }, controller: :photos, action: :destroy
 
