441f38b73643378ba2e81958fd62a51bb64c5f87
d-theus <slma0x02@gmail.com>
2015-11-09 15:49:45 +0300

    cap deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'https://github.com/d-theus/mercedes177.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'WIP'
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
