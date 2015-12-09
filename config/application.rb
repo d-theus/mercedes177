a1cb419e44a2a54e99e23ef0159d1f0cae541849
d-theus <slma0x02@gmail.com>
2015-12-09 18:50:47 +0300

    okzoom.min.js: /public -> /app/assets

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Mercedes
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
+    config.assets.precompile += %w(*.js *.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.assets.paths << "#{Rails}/vendor/assets/twitter/bootstrap/fonts"
   end
