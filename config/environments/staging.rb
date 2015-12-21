f4708f77aaa5fb2178d1532d7a1c6c16280e4852
d-theus <slma0x02@gmail.com>
2015-12-21 16:07:56 +0300

    staging env

---
+++ config/environments/staging.rb
@@ -0,0 +1,16 @@
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.serve_static_assets = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :yui
+  config.assets.digest = true
+  config.log_level = :info
+  config.action_mailer.perform_deliveries = false
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.log_formatter = ::Logger::Formatter.new
+  config.active_record.dump_schema_after_migration = false
+end
